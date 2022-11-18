87ebd70c2f61ceabd68e56689bab49e26eb4ab60
Yuta <yutakato1@gmail.com>
2022-11-18 21:34:54 +0900

    Add files via upload

---
+++ illuminati.sql
@@ -0,0 +1,124 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "illuminati" (
+	"FirstName"	TEXT,
+	"field2"	TEXT,
+	"LastName"	TEXT,
+	"Title"	TEXT,
+	"Genre"	TEXT,
+	"Born"	TEXT,
+	"Birthplace"	TEXT,
+	"Current"	TEXT,
+	"Party"	TEXT,
+	"Denomination"	TEXT,
+	"Ethnicity"	TEXT,
+	"BADegree"	TEXT,
+	"BAgraduationyear"	INTEGER,
+	"HighSchool"	TEXT,
+	"HSgraduationyear"	TEXT,
+	"GraduateDegree"	TEXT,
+	"Gradgraduationyear"	INTEGER,
+	"Family"	TEXT,
+	"Facts"	TEXT
+);
+INSERT INTO "illuminati" VALUES ('Joseph','Robinette','Biden Jr.','POTUS','Politics',NULL,NULL,NULL,NULL,'Catholic',NULL,'University of Delaware',1965,'Archmere Academy, DE','1961','Syracuse University College of Law',1968,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Barack',NULL,'Obama','Former POTUS','Politics',NULL,NULL,NULL,NULL,'Christian',NULL,'Columbia University',1983,'Punahou School, HI','1979','Harvard Law School',1991,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('George','Walker','Bush Jr.','Former POTUS','Politics',NULL,NULL,NULL,NULL,'Christian',NULL,'Yale University',1968,'Phillips Academy, MA',NULL,'Harvard Business School',1975,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('William','Jefferson','Clinton','Former POTUS','Politics',NULL,NULL,NULL,NULL,'Baptist',NULL,'Georgetown University',1968,'Hot Springs High School, AK','1964','Yale Law School',1973,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Hillary','Diane','Clinton','Former Senator','Politics',NULL,NULL,NULL,NULL,'Methodist',NULL,'Wellesley College',1969,'Maine South High School, ME','1965','Yale Law School',1973,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Nancy','Patricia','Pelosi','US Speaker of House','Politics','3/26/1940','Baltimore, MD, US',NULL,NULL,'Catholic',NULL,'Trinity College, DC',1962,'Institute of Notre Dame','1958',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Charles','Ellis','Schumer','US Senate Majority Leader','Politics','11/23/1950','Brooklyn, NY',NULL,NULL,'Jewish',NULL,'Harvard University',1971,'James Madison High School, NY','1967','Harvard Law School',1974,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Donald',NULL,'Trump','Former POTUS','Politics','7/14/1946','Queens, NY',NULL,NULL,'Christian',NULL,'Wharton, University of Pennsylvania',1968,'New York Military Academy',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Bernie',NULL,'Sanders','US Senate','Politics',NULL,NULL,NULL,NULL,'Jewish',NULL,'University of Chicago',1964,'James Madison High School, NY','1959',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Ruth','Bader','Ginsburg','Former SCOTUS','Politics','3/15/1933','Brooklyn, NY',NULL,NULL,'Jewish',NULL,'Cornell University',NULL,'James Madison High School, NY','1950','Columbia Law School',1959,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('George','HW','Bush','Former POTUS','Politics','6/12/1924','Milton, MA',NULL,NULL,'Christian',NULL,'Yale University',1948,'Phillips Academy, MA','1942',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Mark','Elliot','Zuckerberg','CEO META','Corporate',NULL,NULL,NULL,NULL,'Jewish',NULL,NULL,NULL,'Phillips Exeter Academy, NH',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Kamala',NULL,'Harris','Vice POTUS','Politics',NULL,NULL,NULL,NULL,'Baptist',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Stephen',NULL,'Schwarzman','CEO Blackstone','Corporate','2/14/1947',NULL,NULL,NULL,'Jewish',NULL,'Yale University',1969,'Abington Senior High School, PA',NULL,'Harvard Business School',1972,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Lloyd',NULL,'Blankfein','CEO Goldman Sachs','Corporate',NULL,NULL,NULL,NULL,'Jewish',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Rupert',NULL,'Murdoch','News Corp','Media',NULL,NULL,NULL,NULL,'Christian',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('John',NULL,'Roberts','SCOTUS','Politics','1/27/1955','Buffalo, NY',NULL,'Republican','Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Clarence',NULL,'Thomas','SCOTUS','Politics','6/23/1948','Pin Point, GA',NULL,'Republican','Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Samuel',NULL,'Alito','SCOTUS','Politics','4/1/1950','Trenton, NJ',NULL,'Republican','Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Sonia',NULL,'Sotomayor','SCOTUS','Politics','6/25/1954','Bronx, NY',NULL,'Democrat','Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Elena',NULL,'Kagan','SCOTUS','Politics, Academia','4/28/1960','Manhattan, NY',NULL,'Democrat','Jewish',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Neil',NULL,'Gorsuch','SCOTUS','Politics','8/29/1967','Denver, CO',NULL,'Republican','Christian',NULL,NULL,NULL,'Georgetown Preparatory School','1985',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Brett',NULL,'Kavanaugh','SCOTUS','Politics','2/12/1965','Washington, D.C.',NULL,'Republican','Catholic',NULL,NULL,NULL,'Georgetown Preparatory School','1983',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Vivian',NULL,'Barrett','SCOTUS','Politics','1/28/1972','New Orleans, LA',NULL,'Republican','Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Ketanji',NULL,'Jackson','SCOTUS','Politics','9/14/1970','Washigton, D.C.',NULL,'Democrat','Christian',NULL,'Harvard University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Sam',NULL,'Bankman-Fried','Former CEO FTX','Corporate',NULL,NULL,NULL,NULL,'Jewish',NULL,'MIT',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Tim',NULL,'Cook','CEO APPL','Corporate',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Janet',NULL,'Yellen','Former Fed chair','Politics, Academia',NULL,NULL,NULL,NULL,'Jewish',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Volodymyr',NULL,'Zelensky','President of Ukraine','Politics',NULL,NULL,NULL,NULL,'Jewish',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Satya',NULL,'Nadella','CEO MSFT','Corporate',NULL,NULL,NULL,NULL,NULL,'Indian',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Sundar',NULL,'Pichai','CEO GOOGL','Corporate',NULL,NULL,NULL,NULL,NULL,'Indian',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Warren',NULL,'Buffett','Chairman, CEO BRK.A','Corporate',NULL,NULL,NULL,NULL,NULL,'White',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jensen',NULL,'Huang','CEO NVDA','Corporate',NULL,NULL,NULL,NULL,NULL,'Asian',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Mohammed',NULL,'bin Salman','Crown Prince, PM Saudi Arabia','Politics','8/31/1985','Riyadh, Saudi Arabia',NULL,NULL,'Islamic','Arab','King Saud University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Anthony',NULL,'Albanese','PM Australia','Politics','2/3/1963','New South Wales, Australia',NULL,NULL,NULL,'Irish, Italian','University of Sydney',1984,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jair',NULL,'Bolsonaro','President Brazil','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Justin',NULL,'Trudeau','PM Canada','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Xi',NULL,'Jinping','General Secretary China','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Emmanuel',NULL,'Macron','President France','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Olaf',NULL,'Scholz','Chancellor Germany','Politics',NULL,NULL,NULL,'Social Democratic Party of Germany',NULL,NULL,'University of Hamburg',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Narendra',NULL,'Modi','PM India','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Joko',NULL,'Widodo','President Indonesia','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Giorgia',NULL,'Meloni','PM Italy','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Fumio',NULL,'Kishida','PM Japan','Politics',NULL,NULL,NULL,NULL,NULL,NULL,'Waseda',1982,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Andrés',NULL,'Obrador','President Mexico','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Yoon',NULL,'Suk-yeol','President South Korea','Politics',NULL,NULL,NULL,NULL,'Catholic',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Vladimir',NULL,'Putin','President Russia','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Recep',NULL,'Erdoğan','President Turkey','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Rishi',NULL,'Sunak','PM UK','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Ursula',NULL,'von der Leyen','President European Commission','Politics',NULL,NULL,NULL,'Christian Democratic Union of Germany',NULL,NULL,NULL,NULL,NULL,NULL,'Hannover Medical School',1987,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Bernard',NULL,'Arnault','Chairman, CEO LVMH','Corporate','3/5/1949','Roubaix, France',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Arthur',NULL,'Sulzberger','Chairman NYT','Media',NULL,NULL,NULL,NULL,NULL,'Germany','Brown University',2003,'Ethical Culture Fieldston School, NY',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Rachel',NULL,'Maddow','MSNBC','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Stanford University',1994,'Castro Valley High School, CA',NULL,'DPhil, Oxford University',2001,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Tucker',NULL,'Carlson','FOX','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Adam',NULL,'Silver','Comissioner NBA','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Joe',NULL,'Tsai','Owner Nets','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Anderson',NULL,'Cooper','CNN','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Laura',NULL,'Ingraham','FOX','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Kathleen',NULL,'Kingsbury','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Georgetown University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jyoti',NULL,'Thottam','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Yale University',1993,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Binyamin',NULL,'Appelbaum','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'University of Pennsylvania',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Greg',NULL,'Bensinger','Editorial board, NYT','Media',NULL,NULL,'San Francisco, US',NULL,NULL,NULL,'University of Virginia',NULL,NULL,NULL,'Journalism, Columbia University',NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Michelle',NULL,'Cottle','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Vanderbilt University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Mara',NULL,'Gay','Editorial board, NYT','Media',NULL,NULL,'Brooklyn, US',NULL,NULL,NULL,'University of Michigan',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jeneen',NULL,'Interlandi','Editorial board, NYT','Media',NULL,NULL,'Manhattan, US',NULL,NULL,NULL,'Rutgers University',NULL,NULL,NULL,'Columbia University',NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Lauren',NULL,'Kelley','Editorial board, NYT','Media',NULL,NULL,'Brooklyn, US',NULL,NULL,NULL,'Texas Christian University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Alex',NULL,'Kingsbury','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'George Washington University',NULL,NULL,NULL,'Columbia University',NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Serge',NULL,'Schmemann','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Harvard University',1967,'Collegiate School','1963','Columbia University',1971,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Brent',NULL,'Staples','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Widener University',1973,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Farah',NULL,'Stockman','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Harvard University',1996,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jesse',NULL,'Wegman','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Wesleyan University',1996,NULL,NULL,'NYU Law School',2005,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Nick',NULL,'Fox','Editorial board, NYT','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Binghamton University',NULL,NULL,NULL,'Journalism, Columbia University',NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Lee',NULL,'Bollinger','President, Columbia University + Pulitzer Prize Board 2022-2023','Academia, Media',NULL,NULL,NULL,NULL,NULL,NULL,'University of Oregon',1968,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Elizabeth',NULL,'Alexander','President, Andrew W. Mellon Foundation + Pulitzer Prize Board 2022-2023','Academia, Media',NULL,NULL,NULL,NULL,NULL,NULL,'Boston University',1984,'Sidwell Friends School','1980',NULL,NULL,'Clifford Alexander Jr, Mark C. Alexander','Recited poem at 2009 inauguration');
+INSERT INTO "illuminati" VALUES ('Antony',NULL,'Blinken','US Secretary of State','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Lloyd',NULL,'Austin','Secretary of Defense','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Brian',NULL,'Deese','Director, National Economic Council','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Ron',NULL,'Klain','Biden Chief of Staff','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Cecilia',NULL,'Rouse','Chair, Council of Economic Advisors','Politics, Academia',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Linda',NULL,'Thomas-Greenfield','US Ambassador to the UN','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Christina',NULL,'Paxson','President, Brown University','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Swarthmore College',1982,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Martha',NULL,'Pollack','President, Cornell University','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Dartmouth College',1979,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Philip',NULL,'Hanlon','President, Dartmouth College','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Dartmouth College',1977,NULL,NULL,'MIT',1981,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jerome',NULL,'Powell','16th Chair Fed','Politics, Corporate','Washington, D.C.',NULL,NULL,NULL,NULL,NULL,'Princeton University',1975,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('John',NULL,'Dingell','Former Congressman','Politics',NULL,NULL,NULL,NULL,NULL,NULL,'Georgetown University',1949,'Georgetown Preparatory School',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Hanna',NULL,'Gray','Former President, University of Chicago','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Bryn Mawr College',1950,'Sidwell Friends School','1948',NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('John',NULL,'Deutch','Former DCI','Academia, Politics',NULL,NULL,NULL,NULL,'Jewish',NULL,'Amherst College',NULL,'Sidwell Friends School',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('William','Joseph','Burns','Director, CIA','Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Anne',NULL,'Applebaum','Staff writer, The Atlantic','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Yale University',NULL,'Sidwell Friends School',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Zanny','Minton','Beddoes','Editor-in-chief, The Economist',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'University of Oxford',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Sebastian',NULL,'Mallaby','Fellow, Council on Foreign Relations','Media',NULL,NULL,NULL,NULL,NULL,NULL,'University of Oxford',1986,'Eton College',NULL,NULL,NULL,'Christopher Mallaby',NULL);
+INSERT INTO "illuminati" VALUES ('Roula',NULL,'Khalaf',NULL,'Media',NULL,NULL,NULL,NULL,NULL,NULL,'Syracuse University',NULL,NULL,NULL,'Columbia University',NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Ari',NULL,'Emanuel','CEO Endeavor','Entertainment',NULL,NULL,NULL,NULL,'Jewish',NULL,'Macalester College',NULL,NULL,NULL,NULL,NULL,'Rahm Emanuel',NULL);
+INSERT INTO "illuminati" VALUES ('Mary','Elizabeth','Magill','President, University of Pennsylvania','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Yale University',1988,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Laurence',NULL,'Bacow','President, Harvard University','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'MIT',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Christopher',NULL,'Eisgruber','President, Princeton University','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Princeton University',1983,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Peter',NULL,'Salovey','President, Yale University','Academia',NULL,NULL,NULL,NULL,NULL,NULL,'Stanford University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jeff',NULL,'Shell','CEO, NBCUniversal','Media',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Jamal',NULL,'Khashoggi','Journalist, The Washington Post','Media',NULL,NULL,NULL,NULL,NULL,NULL,'Indiana State University',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Rahm',NULL,'Emanuel',NULL,'Politics',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "illuminati" VALUES ('Brian','L','Roberts','Chairman, CEO Comcast','Media',NULL,NULL,NULL,NULL,'Jewish',NULL,'University of Pennsylvania',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+COMMIT;
